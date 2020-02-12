447391a93959d310f19c9df5c71ea453440de86c
Alexander Bondarenko <53271675+Aabondd@users.noreply.github.com>
2020-02-13 01:42:04 +0200

    Add files via upload
    Uploaded hw - 11: Gulp

--- module-11/gulpfile.js
+++ module-11/gulpfile.js
@@ -1,50 +1,137 @@
 'use strict';
 
-const { series, parallel, watch } = require('gulp');
-const requireDir = require('require-dir');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('gulp-cssnano');
+const gcmq = require('gulp-group-css-media-queries');
+const del = require('del');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+const svgstore = require('gulp-svgstore');
+const plumber = require('gulp-plumber');
+const rigger = require('gulp-rigger');
+const stylelint = require('gulp-stylelint');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
+const sequence = require('run-sequence');
 
-const tasks = requireDir('./gulp/tasks', { recurse: true });
-const paths = require('./gulp/paths');
+gulp.task('html', () =>
+  gulp
+    .src('./src/*.html')
+    .pipe(rigger())
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+      }),
+    )
+    .pipe(gulp.dest('./build')),
+);
 
-const serve = () => {
-  return browserSync.init({
-    server: 'build',
+gulp.task('styles', () =>
+  gulp
+    .src('./src/scss/styles.scss')
+    .pipe(plumber())
+    .pipe(
+      stylelint({
+        reporters: [{ formatter: 'string', console: true }],
+      }),
+    )
+    .pipe(sass())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gcmq())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(cssnano())
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream()),
+);
+
+gulp.task('scripts', () =>
+  gulp
+    .src('./src/js/**/*.js')
+    .pipe(plumber())
+    .pipe(
+      babel({
+        presets: ['env'],
+      }),
+    )
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(uglify())
+    .pipe(rename('scripts.min.js'))
+    .pipe(gulp.dest('./build/js')),
+);
+
+gulp.task('svg-sprite', () =>
+  gulp
+    .src('./src/img/sprite/**/*.svg')
+    .pipe(
+      svgstore({
+        inlineSvg: true,
+      }),
+    )
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('./build/img')),
+);
+
+gulp.task('images', () =>
+  gulp
+    .src(['./src/img/**/*.{png,jpg,jpeg,svg}', '!./src/img/sprite/**/*.*'])
+    .pipe(
+      imagemin([
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 3 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: false }, { cleanupIDs: false }],
+        }),
+      ]),
+    )
+    .pipe(gulp.dest('./build/img')),
+);
+
+gulp.task('fonts', () =>
+  gulp.src('./src/fonts/**/*.{woff,woff2}').pipe(gulp.dest('./build/fonts')),
+);
+
+gulp.task('watch', () => {
+  gulp.watch('src/**/*.html', ['html']).on('change', browserSync.reload);
+  gulp.watch('src/scss/**/*.scss', ['styles']);
+  gulp.watch('src/js/**/*.js', ['scripts']);
+});
+
+gulp.task('serve', ['styles'], () =>
+  browserSync.init({
+    server: './build',
     notify: false,
-    open: false,
+    open: true,
     cors: true,
     ui: false,
     logPrefix: 'DevServer',
     host: 'localhost',
-    port: process.env.PORT || 1234,
-  });
-};
-
-const watcher = done => {
-  watch(paths.watch.html).on(
-    'change',
-    series(tasks.html, tasks.inject, browserSync.reload),
-  );
-  watch(paths.watch.css).on('change', series(tasks.css, browserSync.reload));
-  watch(paths.watch.js).on('change', series(tasks.scripts, browserSync.reload));
-  watch(paths.watch.images, tasks.images);
-  watch(paths.watch.fonts, tasks.fonts);
-
-  done();
-};
-
-exports.start = series(
-  tasks.clean,
-  tasks.images,
-  parallel(tasks.css, tasks.fonts, tasks.scripts, tasks.html),
-  tasks.inject,
-  watcher,
-  serve,
+    port: 3000,
+  }),
 );
 
-exports.build = series(
-  tasks.clean,
-  tasks.images,
-  parallel(tasks.css, tasks.fonts, tasks.scripts, tasks.html),
-  tasks.inject,
+gulp.task('del:build', () => del('./build'));
+
+gulp.task('prepare', () => del(['**/.gitkeep', 'README.md', 'banner.png']));
+
+gulp.task('build', cb =>
+  sequence(
+    'del:build',
+    'svg-sprite',
+    'images',
+    'fonts',
+    'styles',
+    'html',
+    'scripts',
+    cb,
+  ),
 );
+
+gulp.task('start', cb => sequence('build', 'serve', 'watch'));
